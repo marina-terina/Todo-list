4a17774471344a4d665055574c90015dcf65b2fe
yesol <yeahsol.kwon@gmail.com>
2024-11-19 20:03:31 +1000

    Marking ToDos as Complete

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,6 +1,15 @@
-import './style.css'
+import "./style.css";
 
 function TodoItem(props) {
-  return <div className="todo">{props.todo.text}</div>;
+  return (
+    <div className="todo">
+      {props.todo.text}
+      <div className={`todo ${props.todo.isCompleted ? "complete" : ""}`}>
+        <button onClick={() => props.completeTodo(props.index)}>
+          Complete
+        </button>
+      </div>
+    </div>
+  );
 }
 export default TodoItem;
