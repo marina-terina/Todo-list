4388bded05c6719a6df4ce3596f638ea8e4abb1f
marina-terina <marinat@outlook.com.au>
2024-11-19 19:13:09 +1000

    display todo array

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useState } from 'react';
 import TodoItem from './components/TodoItem';
 
 function App() {
@@ -15,7 +14,9 @@ function App() {
   return (
     <div>
       <h1>My todo list</h1>
-      {todos.map()}
+      {todos.map((todo, index) => (
+              <TodoItem text={todo} key={index} />
+          ))}
     </div>
   );
 }
